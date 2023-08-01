874b621cb08c5654977c245fd81fd32393fbfe60
P4INTIME <141126848+P4INTIME@users.noreply.github.com>
2023-08-01 09:41:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,84 +1,63 @@
 // create web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+// 1. create web server
+// 2. read comments.json
+// 3. parse comments.json
+// 4. get comments
+// 5. post comments
+// 6. delete comments
+// 7. update comments
+// 8. save comments.json
+// 9. listen to port 3000
+
+// 1. create web server
+const http = require('http');
 const fs = require('fs');
-const compression = require('compression');
-const helmet = require('helmet');
-app.use(helmet());
-const indexRouter = require('./routes/index');
-const topicRouter = require('./routes/topic');
-const helmet = require('helmet');
-app.use(helmet());
+const url = require('url');
+const qs = require('querystring');
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(compression());
-app.get('*', function (request, response, next) {
-    fs.readdir('./data', function (error, filelist) {
-        request.list = filelist;
-        next();
-    });
-});
+const app = http.createServer((req, res) => {
+    const _url = req.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    const title = queryData.id;
 
-app.use('/', indexRouter);
-app.use('/topic', topicRouter);
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry cant find that!');
-});
-
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-// Path: public/main.js
-var Links = {
-    setColor: function (color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
-    }
-}
-var Body = {
-    setColor: function (color) {
-        document.querySelector('body').style.color = color;
-    },
-    setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
-    }
-}
-function nightDayHandler(self) {
-    var target = document.querySelector('body');
-    if (self.value === 'night') {
-        Body.setBackgroundColor('black');
-        Body.setColor('white');
-        self.value = 'day';
-        Links.setColor('powderblue');
-    } else {
-        Body.setBackgroundColor('white');
-        Body.setColor('black');
-        self.value = 'night';
-        Links.setColor('blue');
-    }
-}
-// Path: public/css/style.css
-body {
-    margin: 0;
-    padding: 0;
-    background-color: #ffffff;
-    color: black;
-}
-a {
-    color: black;
-    text-decoration: none;
-}
-a:hover {
-    color: blue;
-}
-h1 {
-    font-size:
+    // 2. read comments.json
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readdir('./data', (err, files) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                    res.end('500 Internal Server Error');
+                }
+                fs.readFile(`./data/${files[0]}`, 'utf8', (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                        res.end('500 Internal Server Error');
+                    }
+                    const title = files[0].slice(0, -5);
+                    const description = data;
+                    const template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
+                    res.writeHead(200);
+                    res.end(template);
+                });
+            });
